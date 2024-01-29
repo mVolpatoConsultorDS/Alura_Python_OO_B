bc010e63e63fa9be2da94e30bc38a98d345ad4be
Milton Volpato <volpato.milton@principal.com>
2024-01-29 15:50:27 -0300

    first commit

---
+++ Modelos/restaurante.py
@@ -0,0 +1,35 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
+
+restaurante_praca = Restaurante('Praca', 'Gourmet')
+#restaurante_praca.nome = 'Praca'
+#restaurante_praca.categoria = 'Gourmet'
+restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
+
+#restanrantes = [restaurante_praca, restaurante_pizza]
+
+##print(restanrantes)
+
+##print(dir(restaurante_praca))
+
+#print(vars(restaurante_praca))
+#print(vars(restaurante_pizza))
+
+#print(restaurante_praca)
+#print(restaurante_pizza)
+
+Restaurante.listar_restaurantes()
